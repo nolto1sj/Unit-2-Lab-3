5b0a8fffc1303242194f0e14f87abc3771b1d627
nolto1sj <sjnolton@gmail.com>
2022-07-06 18:01:00 -0400

    UNCOMMENT SOLUTION

--- Program.cs
+++ Program.cs
@@ -4,22 +4,16 @@ while (loop == true)
 {
     Console.WriteLine("Enter a number");
     int input = int.Parse(Console.ReadLine());
-    int total = 0;
-    for (int i = 1; i <= input; i++)
+    for (int i = 0, q = 1, j = 1; j < input; q += i)
     {
-        total += i;
+        j += 1;
+        q = j + q;
+        int sum = q;
+        if (j == input)
+        {
+            Console.WriteLine(sum);
+        }
     }
-    Console.WriteLine(total);
-    //    for (int i = 0, q = 1, j = 1; j < input; q += i) 
-    //    {
-    //        j += 1;
-    //        q = j + q;
-    //        int sum = q;
-    //        if (j == input)
-    //        {
-    //            Console.WriteLine(sum);
-    //        }
-    //    }
     Console.WriteLine("Would you like to continue? y/n");
     string input2 = Console.ReadLine();
     if (input2 == "y" || input2 == "Y")
